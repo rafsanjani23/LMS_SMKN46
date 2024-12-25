49625c22ba45320afbc66f73af728c135d75d927
Muhammad  Rafsanjani Alfarisi <rafsanjanialfarisi21@gmail.com>
2024-12-25 22:21:42 +0700

    perbaikan complete profile username

--- resources/views/student/complete-profile.blade.php
+++ resources/views/student/complete-profile.blade.php
@@ -14,7 +14,7 @@
 							<label class="px-3 font-medium text-[#414141] text-opacity-50" for="fullname">Name<span
 									class="text-red-500">*</span></label>
 							<input id="fullname" name="fullname" type="text" placeholder="Your name"
-								class="w-full py-4 px-3 text-[#414141] text-opacity-50 bg-[#e8e8e8] focus:outline-none rounded-md" minlength="5" maxlength="5" required>
+								class="w-full py-4 px-3 text-[#414141] text-opacity-50 bg-[#e8e8e8] focus:outline-none rounded-md" required>
 							@error('fullname')
 								<div class="text-red-600 text-xs absolute bottom-[-20px]">
 									{{ $message }}
@@ -47,8 +47,7 @@
 							<label class="pl-3 font-medium text-[#414141] text-opacity-50" for="nis">NIS<span
 									class="text-red-500">*</span></label>
 							<input id="nis" name="nis" placeholder="Your NIS"
-								class="w-full py-4 px-3 text-[#414141] text-opacity-50 bg-[#e8e8e8] focus:outline-none rounded-md" minlength="8"
-								maxlength="8" required>
+								class="w-full py-4 px-3 text-[#414141] text-opacity-50 bg-[#e8e8e8] focus:outline-none rounded-md" minlength="5" maxlength="5" required>
 
 							@error('nis')
 								<div class="text-red-600 text-xs absolute bottom-[-20px]">
