b9b12f84d167c9f690e17cafb8628edf796c328e
Muhammad  Rafsanjani Alfarisi <rafsanjanialfarisi21@gmail.com>
2024-12-25 22:49:00 +0700

    update major di models teacher

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -29,7 +29,7 @@ class Teacher extends Model
       $majors = [
          "pplg" => "Pengembangan Perangkat Lunak dan Gim",
          "dkv" => "Desain Komunikasi Visual",
-         "otkp" => "Otomatisasi dan Tata Kelola Perkantoran",
+         "mp" => "Manajemen Perkantoran",
          "akl" => "Akuntansi dan Keuangan Lembaga",
          "bdp" => "Bisnis Daring dan Pemasaran",
       ];
