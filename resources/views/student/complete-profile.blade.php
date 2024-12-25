0ac59d1be1e4634e5e8bd5f8eba6580651f7b283
Muhammad  Rafsanjani Alfarisi <rafsanjanialfarisi21@gmail.com>
2024-12-26 00:07:26 +0700

    update major completeProfile student

--- resources/views/student/complete-profile.blade.php
+++ resources/views/student/complete-profile.blade.php
@@ -69,7 +69,7 @@
 									<option value="dkv">DESAIN KOMUNIKASI VISUAL</option>
 									<option value="akl">AKUNTANSI DAN KEUANGAN LEMBAGA</option>
 									<option value="bdp">BISNIS DARING DAN PEMASARAN</option>
-									<option value="otkp">OTOMATISASI DAN TATA KELOLA PERKANTORAN</option>
+									<option value="mp">MANAJEMEN PERKANTORAN/option>
 								</select>
 								@error('major')
 									<div class="text-red-600 text-xs absolute bottom-[-20px]">
