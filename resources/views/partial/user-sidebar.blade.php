0ac2332ef37c1a2577ee25e27e3949e38b642ba6
Muhammad Raafi Ramadhan <muhammadraafiramadhan@Muhammads-MacBook-Air.local>
2025-05-26 16:56:33 +0700

    Update user-sidebar limit teacher fullname on sidebar [26/05/2025]

--- resources/views/partial/user-sidebar.blade.php
+++ resources/views/partial/user-sidebar.blade.php
@@ -49,7 +49,7 @@
 				<img class="object-cover w-full h-full" src="{{ Storage::url(Auth::user()->getUserProfile()) }}"
 					alt="profile-picture">
 			</div>
-			<h3 class="text-xl font-semibold mt-4">{{ Auth::user()->teacher->fullname ?? 'Belum diisi' }}</h3>
+			<h3 class="text-xl font-semibold mt-4">{{ Auth::user()->teacher->fullname ? \Illuminate\Support\Str::limit(Auth::user()->teacher->fullname, 25) : 'Belum diisi' }}</h3>
 			<p class="text-sm">{{ ucfirst(Auth::user()->role) }}</p>
 			<a class="bg-[#A9BBF4] hover:bg-[#92a1d2] py-3 px-14 text-xl font-semibold mt-4 rounded"
 				href="{{ route('teacher.profile') }}">View
